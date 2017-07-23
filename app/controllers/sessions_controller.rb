0923486530a73d342f6b93fd30dc51a092ddf389
Matthew Cataldi <mcataloe@gmail.com>
2017-07-23 12:22:42 -0500

    Refined the login controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,31 @@
 get "/login" do
-  
+
   erb :"/login"
 end
 
 post "/login" do
-  
-  if params[:password] == "password"
-    @user = User.find_by(email: params[:email])
+  @user = User.find_by(username: params[:username])
+
+  if @user.password == params[:password]
     session[:user_id] = @user.id
-    
+
     redirect "/users"
   else
     @errors = ["Oops, you didn't enter the right password in. Try again"]
     redirect "/login"
   end
-  
+
 end
 
 get "/register" do
-  
+
   erb :"/register"
 end
 
 
 post "/register" do
   @user = User.new(params[:user])
-  
+
   if @user.save
     redirect "/users"
   else
