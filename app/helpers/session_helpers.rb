981ea2fdccff6f2f26b9575d51ea4a77863ba712
Matthew Cataldi <mcataloe@gmail.com>
2017-07-23 14:19:32 +0000

    Deleted practice migration file (Matt)

--- app/helpers/session_helpers.rb
+++ app/helpers/session_helpers.rb
@@ -1,17 +1,3 @@
-# There are three way to display helpers
-
-# module SessionHelpers
-#   # code here
-# end
-
-# helpers SessionHelpers
-
-# helpers do
-  
-# end
-
-# or just list them
-
 def current_user
   @current_user ||= User.find(session[:user_id]) if session[:user_id]
 end
