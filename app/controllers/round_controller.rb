ddfd7ab5a8417f9e2719bf6933e6f03ebce4d8f1
Ravi R Patel <email": null>
2017-07-23 16:56:36 -0500

    Add rounds progress

---
+++ app/controllers/round_controller.rb
@@ -0,0 +1,21 @@
+get '/rounds' do
+  @deck = Deck.find_by(name: "Nighthawks")
+  @round = Round.create(user: current_user, deck: @deck)
+  @round.available_cards = @deck.cards.length
+  @round.guessed_cards = 0
+  until @round.available_cards == 0
+    @deck.cards.each do |card|
+      @card = card
+      if params[:user_input].downcase == card.answer.downcase
+        @correct = true
+      else
+        @correct = false
+      end
+      Guess.create(round: @round, card: card, correct?: @correct)
+      @round.guessed_cards += 1
+    end
+    @round.available_cards -= 1
+  end
+
+  erb :"rounds/show"
+end
